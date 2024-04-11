a50ebe317a31fda600777becad4ecc88d9b2902c
yebin <yebind3308@gmail.com>
2024-04-11 23:36:47 +0900

    day 14

--- Day14/src/App.jsx
+++ Day14/src/App.jsx
@@ -1,13 +1,70 @@
-
 import './App.css'
+import { useRef, useState } from 'react'
+import Editor from './components/Editor'
+import Header from './components/Header'
+import List from './components/List'
+
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: "Todo1",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Todo2",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "Todo3",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    };
+    setTodos([newTodo, ...todos])
+  }
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === targetId) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          }
+        } 
+        return todo;
+      })
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId
+    ))
+  };
 
   return (
-    <>
-     asd
-    </>
-  )
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
 }
 
 export default App
