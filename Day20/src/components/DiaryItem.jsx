be80313a253fc1912eeb397456eb5a82c029117e
yebind <yebind@bizwizsystem.co.kr>
2024-04-18 14:35:12 +0900

    Day 20

---
+++ Day20/src/components/DiaryItem.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { getEmotioImage } from "../util/get-emotion-image";
+import Button from "./Button";
+import "./DiaryItem.css";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotioImage(emotionId)} />
+      </div>
+
+      <div onClick={() => nav(`/diary/${id}`)} className="info_section">
+        <div className="created_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
+      </div>
+
+      <div className="button_section">
+        <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
+        <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
